ee5badca193a722ad7cfec7580514972489e3992
hls <hls@hlslove.com>
2020-10-31 22:28:38 +0800

    66

--- src/hls/xuexi/test.java
+++ src/hls/xuexi/test.java
@@ -1,7 +1,8 @@
 package hls.xuexi;
 
-public class test {
-    public static void main(String[] args) {
+public class test{
 
+    public static void main(String[] args) {
+        System.out.println("hao");
     }
 }
